5413fb9a5eb39cfb742d9647a22bd3ff090e5fc2
Landi <joaomarcospsnbr@gmail.com>
2025-09-04 21:57:09 -0300

    Aula 04/10/2025

--- src/AbertoFechado/ImpostoRJ.java
+++ src/AbertoFechado/ImpostoRJ.java
@@ -4,7 +4,7 @@ public class ImpostoRJ extends Imposto {
 
     @Override
     public Double calcularImposto(Double valorImposto) {
-        if (valorImposto < 5000D) return valorImposto * 0.12;
+        if (valorImposto < 5000D) return super.calcularImposto(valorImposto) * 0.12;
         if (valorImposto < 100000D) return valorImposto * 21.5;
         return valorImposto * 0.10;
     }
